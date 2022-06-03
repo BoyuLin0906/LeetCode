854d4856445523422255efd96ee0c7f6a84c809a
ByouLin0906 <h5611239@gmail.com>
2022-06-03 14:43:28 +0800

    add 'Range Sum Query 2D - Immutable' solutions in LeetCode

---
+++ 304_Range_Sum_Query_2D_Immutable/Solution_2.py
@@ -0,0 +1,23 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        """
+		Runtime: 2482 ms, faster than 33.11% of Python3 online submissions for Range Sum Query 2D - Immutable.
+		Memory Usage: 24.1 MB, less than 99.03% of Python3 online submissions for Range Sum Query 2D - Immutable.
+        """
+        # Prefix Sum - Optimization
+        row_len, col_len = len(matrix), len(matrix[0])
+        self.matrix = [[0 for _ in range(col_len+1)] for _ in range(row_len+1)]
+        
+        for row_idx in range(row_len):
+            for col_idx in range(col_len):
+                self.matrix[row_idx+1][col_idx+1] = matrix[row_idx][col_idx] + self.matrix[row_idx+1][col_idx] +  self.matrix[row_idx][col_idx+1 ] - self.matrix[row_idx][col_idx]
+        
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.matrix[row2+1][col2+1] - self.matrix[row1][col2+1] - self.matrix[row2+1][col1] + self.matrix[row1][col1]
+        
+    
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
